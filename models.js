5b2b9ad886c49fd9d1c1838fcdb246f1a44c376a
X-fherd <x.fherdinand@gmail.com>
2023-08-22 00:41:15 +0000

    Update

--- models.js
+++ models.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const { schema } = mongoose;
 
-const StockSchema = new schema({
-    symbol: {type: String, required: true},
-    likes: {type: String, default: []},
+const StockSchema = new mongoose.Schema({
+    symbol: { type: String, required: true },
+    likes: { type: Array, default: [] },
 });
+
 const Stock = mongoose.model("Stock", StockSchema);
 
 exports.Stock = Stock;
