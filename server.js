4a875e33d9d9b047d5b4c6ccf32585038e5a1400
X-fherd <x.fherdinand@gmail.com>
2023-08-24 21:27:41 +0000

    Update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://code.jquery.com/jquery-2.2.1.min.js"],
       styleSrc: ["'self'"]
     }
   })
